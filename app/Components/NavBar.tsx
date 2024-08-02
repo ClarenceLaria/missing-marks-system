a93498b10da1806d8fd848908ff9dc9328d36be9
ClarenceLaria <clarencelaria@gmail.com>
2024-08-02 17:40:26 +0300

    Profile Dropdown Functionality

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -1,15 +1,29 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import dp from '@/public/images/ProfilePic.jpeg'
 import logo from '@/public/images/scilogo.png'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 export default   function NavBar() { 
   const [visible, setVisible] = useState(false);
   const toggle = () => {
     setVisible((prev) => !prev);
   }    
+
+  const handleClickOutside = (event: { target: any }) => {
+    setVisible(false);
+  };
+  useEffect(() => {
+    if (visible) {
+      document.addEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [visible]);
   return (
     <div className='w-full h-full flex items-center justify-around px-1 sm:px-4 lg:px-8 '>
       <div className='sm:w-1/4  flex justify-start   '>
@@ -22,7 +36,9 @@ export default   function NavBar() {
         <Image src={dp} alt='ProfilePic' className='w-11 h-11 rounded-full cursor-pointer hover:opacity-70' onClick={toggle} width={1400} height={1400}></Image>
       </div>
       <div className={clsx(`absolute right-8 top-14 bg-gray-50 rounded-lg w-32 text-center text-sm`, !visible && 'hidden')}>
+        <Link href='/'>
         <h1 className='py-3 px-3 hover:bg-gray-100 cursor-pointer border-b-2'>Profile</h1>
+        </Link>
         <h1 className='py-3 px-3 hover:bg-gray-100 cursor-pointer'>Logout</h1>
       </div>
     </div>
