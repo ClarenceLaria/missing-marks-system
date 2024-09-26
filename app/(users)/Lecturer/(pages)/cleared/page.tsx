512fd79e2269daf07a0a6d184c4ef47271d3ae5b
ClarenceLaria <clarencelaria@gmail.com>
2024-09-26 16:55:00 +0300

    Cleared missing marks page

---
+++ app/(users)/Lecturer/(pages)/cleared/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Table from '@/app/(users)/Lecturer/Components/Table'
+import Search from '@/app/(users)/Student/Components/Search'
+
+export default function page() {
+  return (
+    <div className='w-full h-full'>
+        <div className='p-10'>
+            <h1 className='text-2xl font-bold'>Cleared Missing Marks</h1>
+            <Search placeholder='Search for a Reported Missing mark...'></Search>
+            <Table></Table>
+        </div>
+    </div>
+  )
+}
