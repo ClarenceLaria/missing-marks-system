635baf658baa11dba48e7122d3bcd78a7e51e7b1
ClarenceLaria <clarencelaria@gmail.com>
2024-10-21 18:07:07 +0300

    fetchUnits Logic changes based on course

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,20 +3,44 @@ import { PrismaClient, Semester } from "@prisma/client"
 
 const prisma = new PrismaClient();
 
-export async function fetchUnits(academicYear: string, yearOfStudy: number, semester: string) {
-    console.log(academicYear, yearOfStudy, semester)
+export async function fetchUnits(email: string, academicYear: string, yearOfStudy: number, semester: string) {
+    console.log(academicYear, yearOfStudy, semester, email)
     const isValidSemester = Object.values(Semester).includes(semester as Semester);
 
     if (!isValidSemester) {
         throw new Error('Invalid semester value');
     }
+    const user = await prisma.student.findUnique({
+        where: {
+            email: email, 
+        },
+        select: {
+            courseId: true,
+        },
+    });
+    if (!user || !user.courseId) {
+        throw new Error('User does not have an associated course');
+    }
+    const courseId = user.courseId;
     try {
         const units = await prisma.unit.findMany({
             where:{
                 academicYear: academicYear,
                 yearOfStudy: yearOfStudy,
                 semester: semester as Semester,
-            }
+                courses: {
+                    some: {
+                        courseId: courseId,
+                    },
+                }
+            },
+            include: {
+                courses:{
+                    select: {
+                        courseId: true,
+                    },
+                },
+            },
         });
         return units;
     } catch (error) {
