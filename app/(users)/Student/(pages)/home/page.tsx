18c35d3502de6833a6073a6f4a94ca2a2a5696c7
ClarenceLaria <clarencelaria@gmail.com>
2024-10-23 18:30:16 +0300

    Fetch MissingMarkReports successfully and reports status introduced

--- app/(users)/Student/(pages)/home/page.tsx
+++ app/(users)/Student/(pages)/home/page.tsx
@@ -1,6 +1,54 @@
-import React from 'react'
+'use client'
+import Loader from '@/app/Components/Loader';
+import { fetchMissingReports } from '@/app/lib/actions';
+import { ExamType, ReportStatus, Semester } from '@prisma/client';
+import { useSession } from 'next-auth/react'
+import { report } from 'process';
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
 
-export default function page() {
+
+interface missingReport {
+    academicYear: string;
+    yearOfStudy: number;
+    semester: Semester;
+    examType: ExamType;
+    lecturerName: string;
+    id: number;
+    unitName: string;
+    unitCode: string;
+    reportStatus: ReportStatus;
+    studentId: number;
+}
+export default function Page() {
+    const [reports, setReports] = useState<missingReport[]>([]);
+    const [loading,setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    const session = useSession();
+    const email = session.data?.user?.email!;
+    
+    useEffect(() => {
+        const handleFetchReports = async () => {
+            if (!email) {
+                setError('Email is required');
+                return;
+              }
+            try{
+                setLoading(true)
+                const fetchedReports = await fetchMissingReports(email);
+                setReports(fetchedReports);
+                setLoading(false)
+                toast.success('Missing Marks Reports Fetched Successfully')
+            }catch{
+                console.error('Error fetching missing marks report:', error)
+                toast.error('Failed to fetch missing marks reports')
+            }
+        }
+        handleFetchReports();
+    }, [email])
+    if (loading) return <Loader/>
+    console.log(reports)
   return (
     <div className='w-full h-full'>
         <div className='w-full py-5 flex flex-row justify-evenly'>
