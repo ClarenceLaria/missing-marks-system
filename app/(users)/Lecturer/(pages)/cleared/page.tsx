21392d94f957dafc26df9989321df55d53de3fae
ClarenceLaria <clarencelaria@gmail.com>
2024-11-01 18:22:11 +0300

    Cleared Reports fetched successfully

--- app/(users)/Lecturer/(pages)/cleared/page.tsx
+++ app/(users)/Lecturer/(pages)/cleared/page.tsx
@@ -1,9 +1,56 @@
-import React,{Suspense} from 'react'
+'use client'
+import React,{Suspense, useEffect, useState} from 'react'
 import Table from '@/app/(users)/Lecturer/Components/Table'
 import Search from '@/app/(users)/Student/Components/Search'
+import { fetchClearedReports } from '@/app/lib/actions';
+import { useSession } from 'next-auth/react';
+import { ExamType, ReportStatus, Semester } from '@prisma/client';
+import Loader from '@/app/Components/Loader';
+import toast from 'react-hot-toast';
 
+interface missingReport{
+  academicYear: string;
+  yearOfStudy: number;
+  semester: Semester;
+  examType: ExamType;
+  lecturerName: string;
+  id: number;
+  unitName: string;
+  unitCode: string;
+  reportStatus: ReportStatus;
+  studentId: number;
+  createdAt: Date;
+}
 const Loading = () => <div>Loading...</div>;
-export default function page() {
+export default function Page() {
+  const [reports, setReports] = useState<missingReport[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const session = useSession();
+  const email = session.data?.user?.email!;
+  useEffect(() => {
+    const handlefetchReports = async () => {
+      try {
+        const reports = await fetchClearedReports(email);
+        setReports(reports ?? []);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching reports:', error)
+        toast.error('Failed to fetch Cleared Missing Marks')
+      }
+    }
+    handlefetchReports();
+  }, [email])
+
+  const transformedReports = reports.map(report => ({
+    id: report.id,
+    title: report.unitName,
+    unitCode: report.unitCode,
+    date: report.createdAt,
+    status: report.reportStatus,
+  }));
+
+  if(loading) return <Loader/>
   return (
     <div className='w-full h-full'>
         <div className='p-10'>
@@ -12,7 +59,7 @@ export default function page() {
               <Search placeholder='Search for a Cleared Missing mark...'></Search>
             </Suspense>
             <Suspense fallback={<Loading/>}>
-              {/* <Table></Table> */}
+              <Table reports={transformedReports}></Table>
             </Suspense>
         </div>
     </div>
