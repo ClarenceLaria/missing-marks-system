30dcbaff0ac614d3b392cba56defbd6903de8783
ClarenceLaria <clarencelaria@gmail.com>
2024-11-01 06:42:59 +0300

    Updated the submissions page

--- app/(users)/Lecturer/(pages)/submissions/page.tsx
+++ app/(users)/Lecturer/(pages)/submissions/page.tsx
@@ -1,9 +1,56 @@
-import React, {Suspense} from 'react'
+'use client'
+import React, {Suspense, useEffect, useState} from 'react'
 import Table from '@/app/(users)/Lecturer/Components/Table'
 import Search from '@/app/(users)/Student/Components/Search'
+import { ExamType, ReportStatus, Semester } from '@prisma/client';
+import { fetchLecturerMissingMarks } from '@/app/lib/actions';
+import { useSession } from 'next-auth/react';
+import toast from 'react-hot-toast';
+import Loader from '@/app/Components/Loader';
 
 const Loading = () => <div>Loading...</div>;
-export default function page() {
+interface missingReport {
+  academicYear: string;
+  yearOfStudy: number;
+  semester: Semester;
+  examType: ExamType;
+  lecturerName: string;
+  id: number;
+  unitName: string;
+  unitCode: string;
+  reportStatus: ReportStatus;
+  studentId: number;
+  createdAt: Date;
+}
+export default function Page() {
+  const [reports, setReports] = useState<missingReport[]>([]);
+  const [loading,setLoading] = useState(true);
+
+  const session = useSession();
+  const email = session.data?.user?.email!; 
+  useEffect(() => {
+    const handleFetchReports = async () => {
+        try{
+            setLoading(true)
+            const fetchedReports = await fetchLecturerMissingMarks(email);
+            setReports(fetchedReports);
+            setLoading(false)
+            toast.success('Missing Marks Reports Fetched Successfully')
+        }catch(error){
+          toast.error('Failed to fetch missing marks reports')
+          console.error('Error fetching missing marks report:', error)
+        }
+    }
+    handleFetchReports();
+  }, [email])
+  if (loading) return <Loader/>
+  const transformedReports = reports.map(report => ({
+    id: report.id,
+    title: report.unitName,
+    unitCode: report.unitCode,
+    date: report.createdAt,
+    status: report.reportStatus,
+  }));
   return (
     <div className='w-full h-full'>
         <div className='p-10'>
@@ -12,7 +59,7 @@ export default function page() {
               <Search placeholder='Search for a Reported Missing mark...'></Search>
             </Suspense>
             <Suspense fallback={<Loading/>}>
-              <Table></Table>
+              <Table reports={transformedReports}></Table>
             </Suspense>
         </div>
     </div>
