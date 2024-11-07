882c053fd85c4c2040fba04afa98fd28dcdbbc2d
ClarenceLaria <clarencelaria@gmail.com>
2024-11-07 07:57:58 +0300

    Fetch Department Students

--- app/(users)/cod/(pages)/students/page.tsx
+++ app/(users)/cod/(pages)/students/page.tsx
@@ -1,9 +1,48 @@
-import React, {Suspense} from 'react'
+'use client'
+import React, {Suspense, useEffect, useState} from 'react'
 import Table from '@/app/(users)/Admin/Components/UserTable'
 import Search from '@/app/(users)/Student/Components/Search'
+import { useSession } from 'next-auth/react';
+import { fetchDepartmentUsers } from '@/app/lib/actions';
+import { UserType } from '@prisma/client';
+import Loader from '@/app/Components/Loader';
 
+interface Student {
+  id: number;
+  createdAt: Date;
+  email: string;
+  firstName: string;
+  secondName: string;
+  password: string;
+  regNo: string;
+  departmentId: number;
+  courseId: number;
+  userType: UserType;
+}
 const Loading = () => <div>Loading...</div>;
-export default function page() {
+export default function Page() {
+  const [students, setStudents] = useState<Student[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const session = useSession();
+  const email = session.data?.user?.email!;
+  useEffect(() => {
+    const handleFetchStudents = async () => {
+      setLoading(true);
+      const users = await fetchDepartmentUsers(email);
+      setStudents(users.students);
+      setLoading(false);
+    }
+    handleFetchStudents();
+  },[email])
+  if (loading) return <Loader/>;
+
+  const transformedStudents = students.map(student => ({
+    name: student.firstName + " " + student.secondName,
+    email: student.email,
+    regNo: student.regNo,
+    role: student.userType,
+  }))
   return (
     <div className='w-full h-full'>
         <div className='p-10'>
@@ -11,7 +50,7 @@ export default function page() {
               <Search placeholder='Search for a User...'></Search>
             </Suspense>
             <Suspense fallback={<Loading/>}>
-              <Table></Table>
+              <Table students={transformedStudents}></Table>
             </Suspense>
         </div>
     </div>
