e61068adeb9489f1a12baea7b90c52a66f2f167c
ClarenceLaria <clarencelaria@gmail.com>
2024-11-15 14:07:15 +0300

    Updated the Lecturer's page on Admin side

--- app/(users)/Admin/(pages)/lecturers/page.tsx
+++ app/(users)/Admin/(pages)/lecturers/page.tsx
@@ -3,7 +3,7 @@ import React, {Suspense, useEffect, useState} from 'react'
 import Table from '@/app/(users)/Admin/Components/LecturersTable';
 import Search from '@/app/(users)/Student/Components/Search'
 import { useSession } from 'next-auth/react';
-import { fetchSchoolUsers } from '@/app/lib/actions';
+import { fetchSchoolUsers, fetchUniversityUsers } from '@/app/lib/actions';
 import { UserType } from '@prisma/client';
 import Loader from '@/app/Components/Loader';
 
@@ -26,7 +26,7 @@ export default function Page() {
 
   const session = useSession();
   const email = session.data?.user?.email!;
-
+  const userType = session.data?.userType;
   useEffect(() => {
     const fetchLecturers = async () => {
       try{
@@ -41,6 +41,23 @@ export default function Page() {
     };
     fetchLecturers();
   },[email]);
+
+    // ADMIN Logic
+    const [staff, setStaff] = useState<Lecturer[]>([]);
+    useEffect(() => {
+      const handleFetchStaff = async () => {
+        try{
+          setLoading(true);
+          const result = await fetchUniversityUsers();
+          const staff = result?.lecturers || [];
+          setStaff(staff);
+          setLoading(false);
+        }catch(error){
+          console.error("Error fetching staff: ",error);
+        }
+      };
+      handleFetchStaff();
+    },[])
   if (loading) return <Loader/>;
 
   const transformedLecturers = lecturers.map(lecturer => ({
@@ -61,6 +78,26 @@ export default function Page() {
 
       return matchesSearchTerm;
   });
+
+  //Admin Logic
+  const adminTransformedLecturers = staff.map(lecturer => ({
+    name: lecturer.firstName + " " + lecturer.secondName,
+    email: lecturer.email,
+    phoneNo: lecturer.phoneNumber,
+    role: lecturer.userType,
+  }));
+
+  const adminFilteredLecturers = adminTransformedLecturers.filter(
+    (lecturer) => {
+      const matchesSearchTerm =
+      !searchTerm ||
+      lecturer.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      lecturer.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      lecturer.phoneNo.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      lecturer.role.toLowerCase().includes(searchTerm.toLowerCase());
+
+      return matchesSearchTerm;
+    });
   return (
     <div className='w-full h-full'>
         <div className='p-10'>
@@ -72,9 +109,15 @@ export default function Page() {
                   }}
                 ></Search>            
             </Suspense>
+            {userType === 'DEAN' && (
             <Suspense fallback={<Loading/>}>
               <Table lecturers={filteredLecturers}></Table>
+            </Suspense>)}
+            {userType === 'ADMIN' && (
+            <Suspense fallback={<Loading/>}>
+              <Table lecturers={adminFilteredLecturers}></Table> 
             </Suspense>
+            )}
         </div>
     </div>
   )
